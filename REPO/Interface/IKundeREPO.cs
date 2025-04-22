048136362d81834de8b6d3c6cf0458e4831567fa
nikolaj <nikolajfreeland@gmail.com>
2025-04-22 10:43:04 +0200

    Kunde repository og interface lavet

---
+++ REPO/Interface/IKundeREPO.cs
@@ -0,0 +1,13 @@
+﻿using TANE.Kunde.Api.Model;
+
+namespace TANE.Kunde.Api.REPO.Interface
+{
+    public interface IKundeREPO
+    {
+        Task<List<KundeModel>> GetAllKunderAsync();
+        Task<KundeModel> GetKundeByIdAsync(int id);
+        Task AddKundeAsync(KundeModel kunde);
+        Task UpdateKundeAsync(KundeModel kunde);
+        Task DeleteKundeAsync(int id);
+    }
+}
---
+++ REPO/KundeREPO.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TANE.Kunde.Api.Context;
+using TANE.Kunde.Api.Model;
+using TANE.Kunde.Api.REPO.Interface;
+
+
+namespace TANE.Kunde.Api.REPO
+{
+    public class KundeREPO : IKundeREPO
+    {
+        private readonly KundeDbContext _context;
+        public KundeREPO(KundeDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<KundeModel>> GetAllKunderAsync()
+        {
+            return await _context.Kunder.ToListAsync();
+        }
+        public async Task<KundeModel> GetKundeByIdAsync(int id)
+        {
+            return await _context.Kunder.FindAsync(id);
+        }
+        public async Task AddKundeAsync(KundeModel kunde)
+        {
+            await _context.Kunder.AddAsync(kunde);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateKundeAsync(KundeModel kunde)
+        {
+            _context.Kunder.Update(kunde);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteKundeAsync(int id)
+        {
+            var kunde = await GetKundeByIdAsync(id);
+            if (kunde != null)
+            {
+                _context.Kunder.Remove(kunde);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
