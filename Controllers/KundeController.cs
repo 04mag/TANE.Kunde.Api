44da490dfd0b87dbedb589ea2e470b3f2d0060bb
nikolaj <nikolajfreeland@gmail.com>
2025-04-22 12:19:26 +0200

    lavet Http Kald

--- Controllers/KundeController.cs
+++ Controllers/KundeController.cs
@@ -1,1 +1,71 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TANE.Kunde.Api.Model;
+using TANE.Kunde.Api.REPO.Interface;
+
+namespace TANE.Kunde.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class KundeController : ControllerBase
+    {
+        private readonly IKundeREPO _kundeRepo;
+
+        public KundeController(IKundeREPO kundeRepo)
+        {
+            _kundeRepo = kundeRepo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<KundeModel>>> GetAll()
+        {
+            var kunder = await _kundeRepo.GetAllKunderAsync();
+            return Ok(kunder); // 200 OK
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<KundeModel>> GetById(int id)
+        {
+            var kunde = await _kundeRepo.GetKundeByIdAsync(id);
+            if (kunde == null)
+                return NotFound(); // 404 NotFound
+
+            return Ok(kunde); // 200 OK
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(KundeModel kunde)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState); // 400 Bad Request
+
+            await _kundeRepo.AddKundeAsync(kunde);
+            return Ok(); // 200 OK
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(KundeModel kunde)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState); // 400 Bad Request
+
+            try
+            {
+                await _kundeRepo.UpdateKundeAsync(kunde);
+                return Ok(); // 200 OK
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return Conflict("Concurrency Exception! Kunden blev ændret af en anden. Prøv igen!"); // 409 Conflict
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _kundeRepo.DeleteKundeAsync(id);
+            return Ok(); // 200 OK
+        }
+    }
+}
+
