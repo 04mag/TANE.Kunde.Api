93d0d03b0438c952f541155267eb83309c14b012
nikolaj <nikolajfreeland@gmail.com>
2025-04-22 10:42:22 +0200

    DbContext lavet

---
+++ Context/KundeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TANE.Kunde.Api.Model;
+
+namespace TANE.Kunde.Api.Context
+{
+    public class KundeDbContext : DbContext
+    {
+        public KundeDbContext(DbContextOptions<KundeDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<KundeModel>().ToTable("Kunde");
+
+        }
+
+        public DbSet<KundeModel> Kunder { get; set; } = null!;
+    }
+}
